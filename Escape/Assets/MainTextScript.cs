d7b029ec3fcacdf1d0b589608c1a17a39d67d001
Jason Short <jasonshortphd>
2015-03-17 21:26:30 -0700

    Better state of modules
    Added a stand alone text area for commands, and a location marker that
    works.

--- Escape/Assets/MainTextScript.cs
+++ Escape/Assets/MainTextScript.cs
@@ -8,6 +8,7 @@ public class MainTextScript : MonoBehaviour
 
 	public Text MainText;
 	public Text LocationText;
+	public Text CommandText;
 	public Image MainImage;
 
 	private enum Location { ScienceModule, MainHabitat, TubeNorth, TubeSouth, EscapePod1, EscapePod2, BioLab, Outside };
@@ -25,6 +26,8 @@ public class MainTextScript : MonoBehaviour
 		this.ScienceModuleHatch = HatchState.Closed;
 		this.BiolabHatch = HatchState.Closed;
 		currentLocation = Location.ScienceModule;
+		
+		this.CommandText.text = "Please Wait";
 	}
 	
 	// Update is called once per frame
@@ -57,14 +60,16 @@ public class MainTextScript : MonoBehaviour
 	
 	void LocationScienceModule()
 	{
-		string body = "You are in a science module.";
+		string body = "You are in a science module, there appears to be a very small air leak.  You might want to leave this area and seal it off before you run out of oxygen!";
 	
-		string choices = "\n\ns to " + ((this.ScienceModuleHatch == HatchState.Open) ? "close" : "open")  + " the science module hatch";
+		string choices = "S to " + ((this.ScienceModuleHatch == HatchState.Open) ? "close" : "open")  + " the science module hatch";
 		
 		if( this.ScienceModuleHatch == HatchState.Open )
 			choices += "\nE to move east into main module";
 	
-		this.MainText.text = body + "\n" + GetHatchDescription(this.ScienceModuleHatch, "science module") + choices;
+		this.MainText.text = body + "\n" + GetHatchDescription(this.ScienceModuleHatch, "science module");
+		
+		this.CommandText.text = choices;
 		
 		this.LocationText.text = "Science Module";
 		
@@ -84,28 +89,55 @@ public class MainTextScript : MonoBehaviour
 	
 	void LocationBioLab()
 	{
-		this.MainText.text = "You are in a bio lab that is heavily damaged.  There are broken components all around the module.";
-	
+		string body = "You are in a bio lab that is heavily damaged.  There are broken components all around the module.  There is nothing here you can see to salvage.  Maybe the escape pod is still working?";
+		
 		this.LocationText.text = "Bio Lab";
+	
+		string choices = "B to " + ((this.BiolabHatch == HatchState.Open) ? "close" : "open")  + " the biolab hatch.";
+		
+		if( this.BiolabHatch == HatchState.Open )
+			choices += "\nW to move west (main module).";
+		
+		this.MainText.text = body + "\n" + GetHatchDescription(this.BiolabHatch, "bio lab");
+		
+		this.CommandText.text = choices;
+		
+		this.LocationText.text = "Bio Lab";
+		
+		if( Input.GetKeyDown(KeyCode.B) )
+		{
+			if( this.BiolabHatch == HatchState.Open ) 
+				this.BiolabHatch = HatchState.Closed;
+			else if( this.BiolabHatch == HatchState.Closed ) 
+				this.BiolabHatch = HatchState.Open;			
+		}
+		else if (Input.GetKeyDown(KeyCode.W))
+		{
+			this.currentLocation = Location.MainHabitat;
+		}
+		
 	}
 	
 	void LocationMainHabitat()
 	{
-		string body = "The main module is a mess.  There has clearly been a fire here recently.  Maybe that is why you can't remember what happened?";
+		string body = 	"The main module is a mess.  There has clearly been a fire here recently. " + 
+						"There is a broken datapad floating nearby.";
 		
-		string choices = "\n\nS to " + ((this.ScienceModuleHatch == HatchState.Open) ? "close" : "open")  + " the science module hatch" +
-			"\nb to " + ((this.BiolabHatch == HatchState.Open) ? "close" : "open")  + " the biolab hatch";
+		string choices = "S to " + ((this.ScienceModuleHatch == HatchState.Open) ? "close" : "open")  + " the science module hatch. " +
+			"B to " + ((this.BiolabHatch == HatchState.Open) ? "close" : "open")  + " the biolab hatch.\n";
 		
 		if( this.ScienceModuleHatch == HatchState.Open )
-			choices += "\nW to move west into the science module";
+			choices += "W to move west (science module). ";
 
 		if( this.BiolabHatch == HatchState.Open )
-			choices += "\nE to move east into the biolab module";
+			choices += "E to move east (biolab module).";
 		
 		this.MainText.text = body + "\n" + GetHatchDescription(this.ScienceModuleHatch, "science module") +
-			GetHatchDescription(this.BiolabHatch, "biolab module") + choices;
+			GetHatchDescription(this.BiolabHatch, "biolab module");
 		
 		this.LocationText.text = "Main Module";
+		this.CommandText.text = choices;
+		
 		
 		if( Input.GetKeyDown(KeyCode.S) )
 		{
@@ -144,10 +176,10 @@ public class MainTextScript : MonoBehaviour
 		string body = "You are now very cold.  There is not enough oxygen to support life.  It appears you found a module with a hole in it and have died as a result.";
 				
 		this.LocationText.text = "Frozen Solid";
+				
+		this.CommandText.text = "S to start over";
 		
-		string choices = "\nS to start over";
-		
-		this.MainText.text = body + "\n" + choices;
+		this.MainText.text = body;
 		
 		if( Input.GetKeyDown(KeyCode.S) )
 		{
